58cf717acb9edc831ddaf92b53f85724a037e2e5
zhaohuiyan <1183609408@qq.com>
2018-03-11 22:44:00 +0800

    新增属性动画例子

--- app/src/main/java/com/example/zhaohuiyan/animationplay/attributeAnimator/AttributeActivity.java
+++ app/src/main/java/com/example/zhaohuiyan/animationplay/attributeAnimator/AttributeActivity.java
@@ -1,5 +1,7 @@
 package com.example.zhaohuiyan.animationplay.attributeAnimator;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.FloatEvaluator;
 import android.animation.ObjectAnimator;
@@ -56,6 +58,33 @@ public class AttributeActivity extends BaseActivity {
                 colorAnim.setRepeatCount(ValueAnimator.INFINITE);
                 colorAnim.setRepeatMode(ValueAnimator.REVERSE);
                 colorAnim.start();
+            }
+        });
+        findViewById(R.id.b_set).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * 5秒内对view进行旋转、平移、缩放、透明度变化
+                 */
+                AnimatorSet set = new AnimatorSet();
+                set.playTogether(ObjectAnimator.ofFloat(iv_Y,"rotationX",0,360),
+                        ObjectAnimator.ofFloat(iv_Y,"rotationY",0,360),
+                        ObjectAnimator.ofFloat(iv_Y,"translationX",0,90),
+                        ObjectAnimator.ofFloat(iv_Y,"translationY",0,90),
+                        ObjectAnimator.ofFloat(iv_Y,"scaleX",1,1.5f,1),
+                        ObjectAnimator.ofFloat(iv_Y,"scaleY",1,0.5f,1),
+                        ObjectAnimator.ofFloat(iv_Y,"alpha",1,0.25f,1));
+                set.setDuration(5000).start();
+            }
+        });
+        findViewById(R.id.b_xml).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(AttributeActivity.this,
+                        R.animator.anim_set);
+                set.setTarget(iv_Y);
+                set.setDuration(2000);
+                set.start();
             }
         });
         findViewById(R.id.b_clear).setOnClickListener(new View.OnClickListener() {
